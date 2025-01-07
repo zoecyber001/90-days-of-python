61484903a2a6cec3ca1c2496766fb20b7c77731d
Olaoluwa Aina <110189878+zoecyber001@users.noreply.github.com>
2025-01-07 23:44:28 +0100

    2nd day

---
+++ user_input.py
@@ -0,0 +1,5 @@
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))  # Convert input to integer
+year = 2024 - age  # Perform subtraction
+print(f"Hi {name}, You were born in the year {year}")
