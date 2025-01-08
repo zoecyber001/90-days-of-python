fa3e3fc7ac8536885bf4522c93ca1f74dfc5cc62
Olaoluwa Aina <110189878+zoecyber001@users.noreply.github.com>
2025-01-08 15:05:37 +0100

    3rd Day

---
+++ age_checker.py
@@ -0,0 +1,7 @@
+print(f"Federal Government Giveaway")
+age = int(input("How old are you?: "))
+
+if age < 18:
+	print("Your age doesn't permit you to get this")
+else: 
+	print("We will get back to you")
