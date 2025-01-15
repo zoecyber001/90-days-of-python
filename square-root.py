0e299fa4609d1eddf25df2c277c3087f1df1ca0b
Olaoluwa Aina <110189878+zoecyber001@users.noreply.github.com>
2025-01-15 23:59:19 +0100

    10th-day

--- square-root.py
+++ square-root.py
@@ -1,1 +1,9 @@
 import math
+
+def calculate_square_root(number):
+    return math.sqrt(number)
+
+if __name__ == "__main__":
+    user_input = float(input("Enter a number to calculate its square root: "))
+    result = calculate_square_root(user_input)
+    print(f"The square root of {user_input} is {result}")
